ae18811885b0a9f7893df9734efd7ee795f03b33
JackSparrow7777777 <hleekorea23@gmail.com>
2025-09-07 22:44:41 +0900

    Update news.js

--- netlify/functions/news.js
+++ netlify/functions/news.js
@@ -204,7 +204,7 @@ exports.handler = async (event, context) => {
 
     // Filter to premium sources if requested
     let finalArticles = processedArticles;
-    if (false) {  // Temporarily disable filtering
+    if (sources === 'premium' || sources === 'balanced' || sources === 'mainstream') {
       finalArticles = processedArticles.filter(article => article.isTargetSource);
       console.log(`Filtered from ${processedArticles.length} to ${finalArticles.length} target source articles`);
     }
